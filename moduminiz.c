fb3bfbfa4f5ef88f55a5b4742d5b26c987328389
Damiano Mazzella <damianomazzella@gmail.com>
2022-06-11 15:51:59 +0200

    Removed last param of MP_REGISTER_MODULE

--- moduminiz.c
+++ moduminiz.c
@@ -123,6 +123,6 @@ const mp_obj_module_t mp_module_uminiz = {
 };
 
 // Register the module to make it available in Python
-MP_REGISTER_MODULE(MP_QSTR__miniz, mp_module_uminiz, MICROPY_PY_UMINIZ);
+MP_REGISTER_MODULE(MP_QSTR__miniz, mp_module_uminiz);
 
 #endif // MICROPY_PY_UMINIZ
